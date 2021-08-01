0e4c538806da5c2b6139e55d9761622cda7cdf95
lordchancellor <c.d.j.ford@gmail.com>
2021-08-01 19:25:15 +0100

    Added toolbar colour to all pages

--- src/app/inmates-list/inmates-list.component.ts
+++ src/app/inmates-list/inmates-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Inmate } from '../_models';
